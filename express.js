f3c77d9fee016fbbe1f84bc096a3ededc1fdd837
karenpeng <itismekarenp@gmail.com>
2014-07-31 15:20:27 -0400

    maybe there's something wrong with my computer

--- express.js
+++ express.js
@@ -27,3 +27,39 @@ app.post('/form', function(req, res){
   res.end(result)
 })
 */
+
+/*
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+*/
+
+
+/*
+var stylus = require('stylus');
+
+app.use(stylus.middleware(process.argv[3]));
+app.use(express.static(process.argv[3]));
+*/
+
+//ok, i give up
+/*
+app.put(process.argv[3], function(req, res){
+  req.params.NAME
+});
+*/
+
+/*
+app.get('/search',function(req, res){
+  var data = JSON.stringify(req.query);
+  res.send(data);
+});
+*/
+
+/*
+var fs = require('fs');
+fs.readFile(process.argv[3], function(err, data){
+	if(err) throw err;
+	var obj = JSON.parse(data);
+	var content = res.json(obj);
+	res.send(content);
+});
+*/
